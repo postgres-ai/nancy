5c6e59ae1a9124ea6634ab8ac451e3a94f054087
NikolayS <github2@samokhvalov.com>
2018-06-18 09:38:36 +0300

    better formatting in CI tests

--- tests/nancy_run_no_options.sh
+++ tests/nancy_run_no_options.sh
@@ -3,9 +3,9 @@
 output=$(source "${BASH_SOURCE%/*}/../nancy_run.sh" 2>&1)
 
 if [[ $output =~ "ERROR: AWS keys not given" ]]; then
-  echo "OK!"
+  echo -e "\e[36mOK\e[39m"
 else
-  >&2 echo "FAILED"
+  >&2 echo -e "\e[31mFAILED\e[39m"
 fi
 
 
