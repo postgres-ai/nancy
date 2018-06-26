38c65be242a8997897053242b0f32af9b99e563e
NikolayS <github2@samokhvalov.com>
2018-06-26 09:17:13 +0300

    CI: fix error msg

--- tests/nancy_run_options_no_instance_type.sh
+++ tests/nancy_run_options_no_instance_type.sh
@@ -3,7 +3,7 @@
 params="--run-on aws --aws-keypair-name awskey --aws-ssh-key-path \"/home/someuser/.ssh/awskey.pem\""
 output=$(${BASH_SOURCE%/*}/../nancy_run.sh $params 2>&1)
 
-if [[ $output =~ "ERROR: Instance type not given." ]]; then
+if [[ $output =~ "ERROR: AWS EC2 Instance type not given." ]]; then
   echo -e "\e[36mOK\e[39m"
 else
   >&2 echo -e "\e[31mFAILED\e[39m"
