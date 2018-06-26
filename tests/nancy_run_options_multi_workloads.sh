3a13180fb532e41819ff0b79e4809e1d967bd29a
NikolayS <github2@samokhvalov.com>
2018-06-26 09:07:03 +0300

    CI: better synta inside tests

--- tests/nancy_run_options_multi_workloads.sh
+++ tests/nancy_run_options_multi_workloads.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 params="--run-on aws --aws-keypair-name awskey --aws-ssh-key-path \"/home/someuser/.ssh/awskey.pem\" --aws-ec2-type \"r4.large\" --s3-cfg-path \"/home/someuser/.s3cfg\" --workload-full-path \"s3://somedir/db.sql.30min.pgreplay\" --workload-basis-path \"\" --workload-custom-sql \"select now();\""
-output=$(source "${BASH_SOURCE%/*}/../nancy_run.sh" $params 2>&1)
+output=$(${BASH_SOURCE%/*}/../nancy_run.sh $params 2>&1)
 
 if [[ $output =~ "ERROR: 2 or more workload source given." ]]; then
   echo -e "\e[36mOK\e[39m"
--- tests/nancy_run_options_no_instance_type.sh
+++ tests/nancy_run_options_no_instance_type.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 params="--run-on aws --aws-keypair-name awskey --aws-ssh-key-path \"/home/someuser/.ssh/awskey.pem\""
-output=$(source "${BASH_SOURCE%/*}/../nancy_run.sh" $params 2>&1)
+output=$(${BASH_SOURCE%/*}/../nancy_run.sh $params 2>&1)
 
 if [[ $output =~ "ERROR: Instance type not given." ]]; then
   echo -e "\e[36mOK\e[39m"
