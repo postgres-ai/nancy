976dfbc41d1c08a8969ddec1b72e67b715dc0fa7
NikolayS <github2@samokhvalov.com>
2018-06-26 09:44:49 +0300

    CI: use if-then-fi instead of implicit "if" via "&&"

--- tests/nancy_run_options_no_instance_type.sh
+++ tests/nancy_run_options_no_instance_type.sh
@@ -5,6 +5,7 @@ output=$(${BASH_SOURCE%/*}/../nancy_run.sh $params 2>&1)
 
 if [[ $output =~ "ERROR: AWS EC2 Instance type not given." ]]; then
   echo -e "\e[36mOK\e[39m"
+  exit 0
 else
   >&2 echo -e "\e[31mFAILED\e[39m"
   exit 1
