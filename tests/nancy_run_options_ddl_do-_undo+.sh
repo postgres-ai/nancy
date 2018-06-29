bd606aec3225745a93040c1f6fbca831a276fc12
NikolayS <github2@samokhvalov.com>
2018-06-29 11:05:31 +0300

    CI: static tests must work now

--- tests/nancy_run_options_ddl_do-_undo+.sh
+++ tests/nancy_run_options_ddl_do-_undo+.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-params="--run-on aws --debug --aws-keypair-name awskey --aws-ssh-key-path \"/home/someuser/.ssh/awskey.pem\" --aws-ec2-type \"r4.large\" --s3-cfg-path \"/home/someuser/.s3cfg\" --workload-full-path \"s3://somedir/db.sql.30min.pgreplay\" --pg-version 9.6 --tmp-path \"tmp\" --db-dump-path \"s3://somedir/dump.sql.bz2\" $undo_param--target-ddl-undo \"drop;\" "
 read -r -d '' params <<PARAMS
   --run-on aws --aws-keypair-name awskey --pg-version 9.6 \
   --aws-ssh-key-path "/home/someuser/.ssh/awskey.pem" \
@@ -11,6 +10,7 @@ read -r -d '' params <<PARAMS
   --db-dump-path "s3://somebucket/dump.sql.bz2" \
   --target-ddl-undo "drop index i_zzz;"
 PARAMS
+
 output=$(${BASH_SOURCE%/*}/../nancy_run.sh $params 2>&1)
 
 if [[ $output =~ "ERROR: DDL code must have do and undo part." ]]; then
--- tests/nancy_run_options_multi_workloads.sh
+++ tests/nancy_run_options_multi_workloads.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-params="--run-on aws --aws-keypair-name awskey --aws-ssh-key-path \"/home/someuser/.ssh/awskey.pem\" --aws-ec2-type \"r4.large\" --s3-cfg-path \"/home/someuser/.s3cfg\" --workload-full-path \"s3://somedir/db.sql.30min.pgreplay\" --workload-basis-path \"\" --workload-custom-sql \"select now();\""
+read -r -d '' params <<PARAMS
+  --run-on aws --aws-keypair-name awskey --pg-version 9.6 \
+  --aws-ssh-key-path "/home/someuser/.ssh/awskey.pem" \
+  --aws-ec2-type "r4.large" \
+  --s3cfg-path "/home/someuser/.s3cfg" \
+  --workload-full-path "s3://somebucket/db.sql.30min.pgreplay" \
+  --workload-custom-sql "select now();" \
+  --tmp-path tmp \
+  --db-dump-path "s3://somebucket/dump.sql.bz2"
+PARAMS
+
 output=$(${BASH_SOURCE%/*}/../nancy_run.sh $params 2>&1)
 
-if [[ $output =~ "ERROR: 2 or more workload source given." ]]; then
+if [[ $output =~ "ERROR: 2 or more workload sources are given." ]]; then
   echo -e "\e[36mOK\e[39m"
 else
   >&2 echo -e "\e[31mFAILED\e[39m"
+  >&2 echo -e "Output: $output"
 fi
