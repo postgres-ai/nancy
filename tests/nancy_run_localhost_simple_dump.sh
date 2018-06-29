7757e5632354080cc194a4edf9d01fd40dc27ab7
NikolayS <github2@samokhvalov.com>
2018-06-29 06:57:30 +0300

    attempt to fix "local" tests

--- tests/nancy_run_localhost_simple_dump.sh
+++ tests/nancy_run_localhost_simple_dump.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-set -ueo pipefail
-
 thisDir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 parentDir="$(dirname "$thisDir")"
 srcDir="$parentDir/.circleci"
-#bzip2 "$srcDir/test.dump"
 if [ ! -d "$srcDir/tmp" ]; then
   mkdir "$srcDir/tmp"
 fi
 nancyRun="$parentDir/nancy_run.sh"
 
 output=$(
-  $nancyRun --workload-custom-sql $srcDir/custom.sql --db-dump-path $srcDir/test.dump.bz2 \
-    --tmp-path $srcDir/tmp --debug 2>&1
+  $nancyRun --workload-custom-sql "file://$srcDir/custom.sql" \
+    --db-dump-path "file://$srcDir/test.dump.bz2" \
+    --tmp-path $srcDir/tmp
 )
 
 if [[ $output =~ "Queries duration:" ]]; then
