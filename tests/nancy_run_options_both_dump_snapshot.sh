6ef499eb244d37dcc03bc51838b778e4c6228906
NikolayS <github2@samokhvalov.com>
2018-06-29 07:24:20 +0300

    refactor one test, attempt to make it work

--- tests/nancy_run_options_both_dump_snapshot.sh
+++ tests/nancy_run_options_both_dump_snapshot.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-params=<<PARAMS
+read -r -d '' params <<PARAMS
   --run-on aws --aws-keypair-name awskey --pg-version 9.6 \
   --aws-ssh-key-path "/home/someuser/.ssh/awskey.pem" \
   --aws-ec2-type "r4.large" \
   --s3cfg-path "/home/someuser/.s3cfg" \
-  --workload-full-path "s3://somedir/db.sql.30min.pgreplay" \
-  --tmp-path tmp
-  --db-dump-path "s3://somedir/dump.sql.bz2"
-  --db-prepared-snapshot "s3://somdir/snapshot"
+  --workload-full-path "s3://somebucket/db.sql.30min.pgreplay" \
+  --tmp-path tmp \
+  --db-dump-path "s3://somebucket/dump.sql.bz2" \
+  --db-prepared-snapshot "s3://somebucket/snapshot"
 PARAMS
 
 output=$(${BASH_SOURCE%/*}/../nancy_run.sh $params 2>&1)
 
-if [[ $output =~ "ERROR: Both snapshot and dump sources given." ]]; then
+if [[ $output =~ "ERROR: Both snapshot and dump sources are given." ]]; then
   echo -e "\e[36mOK\e[39m"
 else
   >&2 echo -e "\e[31mFAILED\e[39m"
