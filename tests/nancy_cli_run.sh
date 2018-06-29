a3ecb7287072310eddcdb5674b2cb4c7a43a4a98
NikolayS <github2@samokhvalov.com>
2018-06-29 07:10:18 +0300

    CI: accumulate errors

--- tests/nancy_cli_run.sh
+++ tests/nancy_cli_run.sh
@@ -2,7 +2,7 @@
 
 export PATH=$PATH:${BASH_SOURCE%/*}/..
 
-output=$(nancy run --run-on aws)
+output=$(nancy run --run-on aws 2>&1)
 
 if [[ $output =~ "ERROR: AWS keys not given" ]]; then
   echo -e "\e[36mOK\e[39m"
