85f691e637d18c3e92e6ae7bd7825286142b826d
NikolayS <github2@samokhvalov.com>
2018-07-23 09:55:49 +0300

    CI: attempt to make "real workload" test to work

--- tests/nancy_run_localhost_real_workload.sh
+++ tests/nancy_run_localhost_real_workload.sh
@@ -15,7 +15,7 @@ output=$(
     --tmp-path $srcDir/tmp 2>&1
 )
 
-if [[ $output =~ "Queries:           1" ]]; then
+if [[ $output =~ "Queries:[[:space:]]+1" ]]; then
   echo -e "\e[36mOK\e[39m"
 else
   >&2 echo -e "\e[31mFAILED\e[39m"
