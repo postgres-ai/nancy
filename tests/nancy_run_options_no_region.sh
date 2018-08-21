2b556b1b3b543350ebd9dce629b238f604cb3abc
dmius <uddm@mail.ru>
2018-08-21 22:56:21 +0300

    Tests fix

--- tests/nancy_run_options_no_region.sh
+++ tests/nancy_run_options_no_region.sh
@@ -4,15 +4,16 @@ read -r -d '' params <<PARAMS
   --run-on aws --aws-keypair-name awskey --pg-version 9.6 \
   --aws-ssh-key-path "/home/someuser/.ssh/awskey.pem" \
   --aws-ec2-type "r4.large" \
-  --aws-region "us-east-1" \
   --s3cfg-path "/home/someuser/.s3cfg" \
   --workload-real "s3://somebucket/db.sql.30min.pgreplay" \
-  --tmp-path tmp
+  --tmp-path tmp \
+  --db-dump "s3://somebucket/dump.sql.bz2" \
+  --db-prepared-snapshot "s3://somebucket/snapshot"
 PARAMS
 
 output=$(${BASH_SOURCE%/*}/../nancy_run.sh $params 2>&1)
 
-if [[ $output =~ "ERROR: The object (database) is not defined." ]]; then
+if [[ $output =~ "ERROR: AWS EC2 region not given." ]]; then
   echo -e "\e[36mOK\e[39m"
 else
   >&2 echo -e "\e[31mFAILED\e[39m"
