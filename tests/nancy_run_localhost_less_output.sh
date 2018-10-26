bf1358303885b74ecc2a8b54fe616ac3d29ef13d
dmius <uddm@mail.ru>
2018-10-26 14:24:06 +0300

    Less output test

---
+++ tests/nancy_run_localhost_less_output.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+realpath() {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+src_dir=$(dirname $(dirname $(realpath "$0")))"/.circleci"
+
+output=$(
+  ${BASH_SOURCE%/*}/../nancy run \
+    --less-output \
+    --db-pgbench "-s 1" \
+    --workload-pgbench "-t 1" 2>&1
+)
+
+if ! [[ $output =~ "LOG: Ok, generating html report..." ]]; then
+  echo -e "\e[36mOK\e[39m"
+else
+  >&2 echo -e "\e[31mFAILED\e[39m"
+  >&2 echo -e "Output: $output"
+  exit 1
+fi
