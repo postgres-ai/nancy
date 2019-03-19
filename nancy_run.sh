a1679bdb21159464bdff4e6dc865ed3e9e4cd77f
Nikolay Samokhvalov <gitlab@samokhvalov.com>
2019-03-19 09:44:55 +0000

    Do not hide Docker errors

--- nancy_run.sh
+++ nancy_run.sh
@@ -1217,8 +1217,13 @@ trap cleanup_and_exit 1 2 13 15 EXIT
 
 if [[ "$RUN_ON" == "localhost" ]]; then
   if [[ -z ${CONTAINER_ID+x} ]]; then
+    msg "Pulling the Docker image (postgresmen/postgres-nancy:${PG_VERSION})..."
     docker pull "postgresmen/postgres-nancy:${PG_VERSION}" 2>&1 \
-      | grep -e 'Pulling from' -e Digest -e Status -e Error
+      | grep -v Waiting \
+      | grep -v Pulling \
+      | grep -v Verifying \
+      | grep -v "Already exists" \
+      | grep -v " complete"
 
     if [[ ! -z ${DB_LOCAL_PGDATA+x} ]] || [[ ! -z ${PGDATA_DIR+x} ]]; then
       if [[ ! -z ${DB_LOCAL_PGDATA+x} ]]; then
